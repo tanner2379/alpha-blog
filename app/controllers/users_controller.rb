3fc008e900f7011a91cc7e4e49fff9aa788a359e
Tanner Sigel <tanner2379@live.com>
2020-09-25 12:16:57 -0700

    Added users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
 
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
     def new
         @user = User.new
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Your account information was successfully updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
     end
 
     def create
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    #uses gravatar, change to use saved image
+    def avatar_for(user, options = {size: 80})
+        email_address = user.email.downcase
+        hash = Digest::MD5.hexdigest(email_address)
+        size = options[:size]
+        avatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+        image_tag(avatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+    end
 end
