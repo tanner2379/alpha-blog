1bdeee45447b3eb0239c62b27c24d1e31ce82ca3
Tanner Sigel <tanner2379@live.com>
2020-09-25 21:28:30 -0700

    Added admin functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -58,7 +58,7 @@ before_action :require_same_user, only: [:edit, :update, :destroy]
     end
     
     def require_same_user
-        if current_user != @article.user
+        if (current_user != @article.user && !current_user.admin?)
             flash[:alert] = "You can only edit or delete your own article"
             redirect_to @article
         end
