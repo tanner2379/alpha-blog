475f5f4e7b07b7efc74839cde37c08c34d887577
Tanner Sigel <tanner2379@live.com>
2020-09-27 14:34:47 -0700

    Added Categories and automated testing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ before_action :require_same_user, only: [:edit, :update, :destroy]
     end
 
     def article_params
-        params.require(:article).permit(:title, :description)
+        params.require(:article).permit(:title, :description, category_ids: [])
     end
     
     def require_same_user
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   # above is how you expose routes individually
   resources :articles
   resources :users, except: [:new]
+  resources :categories, except: [:destroy]
 end
